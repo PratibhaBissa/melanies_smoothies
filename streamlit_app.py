97f40a6fa01d5e97f2952d5f9b38ddb7fa761dbf
PratibhaBissa <bissapratibha16@gmail.com>
2026-05-17 01:02:10 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ session = cnx.session()
 name_on_order = st.text_input("Name on Smoothie:")
 st.write("The name on your Smoothie will be:", name_on_order)
 
-my_dataframe = session.sql("SELECT FRUIT_NAME,SEARCH_ON FROM smoothies.public.fruit_options")\
+my_dataframe = session.sql("SELECT FRUIT_NAME,SEARCH_ON FROM smoothies.public.fruit_options")
 pd_df=my_dataframe.to_pandas()
 st.dataframe(pd_df)
 
