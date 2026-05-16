4bbfff79b97295d199ddf88db3d0b688f01d4d9f
PratibhaBissa <bissapratibha16@gmail.com>
2026-05-17 00:59:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ session = cnx.session()
 name_on_order = st.text_input("Name on Smoothie:")
 st.write("The name on your Smoothie will be:", name_on_order)
 
-my_dataframe = session.sql("SELECT FRUIT_NAME FROM smoothies.public.fruit_options")
+my_dataframe = session.sql("SELECT FRUIT_NAME,SEARCH_ON FROM smoothies.public.fruit_options")
 
 ingredients_list = st.multiselect('Choose up to 5 ingredients:', my_dataframe, max_selections=5)
 
@@ -28,8 +28,9 @@ if ingredients_list:
         st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅") 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ''
+        search_on=pd_df.loc(pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         st.subheader(fruit_chosen + ' Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+ fruit_chosen)  
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/{search_on}")  
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 
