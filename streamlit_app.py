97d9015097e6e4e4d9f3eac5927aeb1732c05347
PratibhaBissa <bissapratibha16@gmail.com>
2026-05-17 00:13:35 +0530

    Refactor smoothie order insertion logic

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,47 +1,27 @@
-# Import python packages.
 import streamlit as st
-
-
 
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 st.write("Choose the fruits you want in your custom Smoothie!")
 
-
-
 cnx = st.connection("snowflake")
 session = cnx.session()
-
-
 
 name_on_order = st.text_input("Name on Smoothie:")
 st.write("The name on your Smoothie will be:", name_on_order)
 
-
-
 my_dataframe = session.sql("SELECT FRUIT_NAME FROM smoothies.public.fruit_options")
-
-
 
 ingredients_list = st.multiselect('Choose up to 5 ingredients:', my_dataframe, max_selections=5)
 
-
-
 if ingredients_list:
-   
-    ingredients_string = ''.join(ingredients_list)
+    ingredients_string = ' '.join(ingredients_list)
 
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
+                values ('""" + ingredients_string + """','""" + name_on_order + """')"""
+    st.write(my_insert_stmt)
 
+    time_to_insert = st.button('Submit Order')
 
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
-                values ('""" + ingredients_string + """','""" + name_on_order + """')"""
-    st.write(my_insert_stmt)
-
-
-
-    time_to_insert = st.button('Submit Order')
-
-
-
-    if time_to_insert:
-        session.sql(my_insert_stmt).collect()
-        st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
+    if time_to_insert:
+        session.sql(my_insert_stmt).collect()
+        st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
